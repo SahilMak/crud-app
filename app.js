0c37c48da5547ab30af6bdec61b078f587afaf5f
Sahil Makhijani <svmakhijani94@gmail.com>
2017-04-13 21:28:06 -0400

    Added link & content to new view

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var robots_index = require('./routes/robots/index');
+var robots_new = require('./routes/robots/new');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/robots', robots_index);
+app.use('/robots/new', robots_new);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/robots/new.js
+++ routes/robots/new.js
@@ -4,10 +4,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.render('robots/index', {
-        //
-    });
+router.get('/', function(req, res) {
+    res.render('robots/new');
 });
 
 module.exports = router;
