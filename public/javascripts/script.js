514c0909b550dedd2782c1cfa494638cc845df9f
Sahil Makhijani <svmakhijani94@gmail.com>
2017-04-20 23:17:50 -0400

    Added script file to be used for posting data

---
+++ public/javascripts/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Sahil Makhijani on 4/20/2017.
+ */
+var name = document.getElementById("name");
+var description = document.getElementById("description");
+var stock = document.getElementById("stock");
+var submit = document.getElementById("submit");
+
+submit.addEventListener("click", function (event) {
+    //
+}, false);
