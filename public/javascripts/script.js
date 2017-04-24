50b77bdc0d39a3f14d8f26adc793682c0af7dfa5
Sahil Makhijani <svmakhijani94@gmail.com>
2017-04-24 02:10:03 -0400

    Delete robot function added

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,16 +4,34 @@
 $(document).ready(function () {
     // Add robot
     $("#newForm").submit(function (event) {
-        console.log("Is anything happening?");
         event.preventDefault();
-        console.log("Anything?");
         var url = "https://southernct-443-robots-api.herokuapp.com/api/robots";
-        console.log("ANYTHING?");
         var form = {name: $("#name").val(), description: $("#description").val(), in_stock: $("#stock").val()};
-        console.log("You made it this far!");
         $.post(url, form)
             .done(function (data, textStatus, xhr) {
-                console.log("Done!");
+                window.location = "/robots";
+            })
+            .fail(function (xhr, textStatus, errorThrown) {
+                alert("ERROR: " + errorThrown);
+            });
+    });
+
+    // Delete robot
+    $(".delete").click(function () {
+        var row = $(this).parent().siblings();
+        $.ajax({
+            method: "DELETE",
+            url: "https://southernct-443-robots-api.herokuapp.com/api/robots/" + row.eq(0).text(),
+            data: {
+                _id: row.eq(0).text(),
+                name: row.eq(1).children().text(),
+                description: row.eq(2).text(),
+                in_stock: row.eq(3).text(),
+                created_at: row.eq(4).text(),
+                updated_at: row.eq(5).text()
+            }
+        })
+            .done(function () {
                 window.location = "/robots";
             })
             .fail(function (xhr, textStatus, errorThrown) {
