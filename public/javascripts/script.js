880b6d95f497fbb31ae6e4a8fe15d330912400e9
Sahil Makhijani <svmakhijani94@gmail.com>
2017-04-24 21:29:15 -0400

    Edit robot function added

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,14 +2,37 @@
  * Created by Sahil Makhijani on 4/20/2017.
  */
 $(document).ready(function () {
+    var url = "https://southernct-443-robots-api.herokuapp.com/api/robots/";
+
     // Add robot
     $("#newForm").submit(function (event) {
         event.preventDefault();
-        var url = "https://southernct-443-robots-api.herokuapp.com/api/robots";
         var form = {name: $("#name").val(), description: $("#description").val(), in_stock: $("#stock").val()};
         $.post(url, form)
             .done(function (data, textStatus, xhr) {
                 window.location = "/robots";
+            })
+            .fail(function (xhr, textStatus, errorThrown) {
+                alert("ERROR: " + errorThrown);
+            });
+    });
+
+    // Edit robot
+    var id = "";
+    $(".edit").click(function () {
+        id = $(this).parent().siblings().eq(0).text();
+        window.location = "/robots/" + id + "/edit";
+    });
+    $("#editForm").submit(function (event) {
+        event.preventDefault();
+        console.log(url + id);
+        $.ajax({
+            method: "PUT",
+            url: url + id,
+            data: {name: $("#name").val(), description: $("#description").val(), in_stock: $("#stock").val()}
+        })
+            .done(function () {
+                window.location = "/robots/" + id;
             })
             .fail(function (xhr, textStatus, errorThrown) {
                 alert("ERROR: " + errorThrown);
@@ -21,7 +44,7 @@ $(document).ready(function () {
         var row = $(this).parent().siblings();
         $.ajax({
             method: "DELETE",
-            url: "https://southernct-443-robots-api.herokuapp.com/api/robots/" + row.eq(0).text(),
+            url: url + row.eq(0).text(),
             data: {
                 _id: row.eq(0).text(),
                 name: row.eq(1).children().text(),
--- routes/robots/edit.js
+++ routes/robots/edit.js
@@ -2,10 +2,22 @@
  * Created by Sahil Makhijani on 4/15/2017.
  */
 var express = require('express');
+var fetch = require('node-fetch');
 var router = express.Router();
 
 router.get('/:id/edit', function(req, res) {
-    res.render('robots/edit');
+    fetch('https://southernct-443-robots-api.herokuapp.com/api/robots/' + req.params.id)
+        .then(function (response) {
+            response.json()
+                .then(function (info) {
+                    res.render('robots/edit', {
+                        robot: info
+                    });
+                });
+        })
+        .catch(function (err) {
+            console.log("ERROR:", err);
+        });
 });
 
 module.exports = router;
