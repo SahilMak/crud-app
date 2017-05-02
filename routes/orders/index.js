4cf90c8e99ac79a9ec710bd8aabcb6ddf071d2a4
Sahil Makhijani <svmakhijani94@gmail.com>
2017-05-02 18:58:55 -0400

    Set up index for orders page

---
+++ routes/orders/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Sahil Makhijani on 5/2/2017.
+ */
+var express = require('express');
+var mongoose = require('mongoose');
+var Orders = mongoose.model('orders');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    Orders.find(function (err, doc) {
+        console.log(doc);
+        res.render('orders/index', {
+            data: doc
+        });
+    });
+});
+
+module.exports = router;
